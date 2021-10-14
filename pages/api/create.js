bd9cb28d4f4eda3aa9d287ae913223eb15d233d1
hmdogan00 <mertdogan@jotform.com>
2021-10-14 23:25:01 +0300

    database connection and api features initialized

---
+++ pages/api/create.js
@@ -0,0 +1,20 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import prisma from "../../lib/prisma";
+
+export default async (req, res) => {
+  if (req.method !== 'POST') return res.status(405).json({ message: 'Method not allowed'});
+
+  try {
+    const {user} = req.body;
+    // const user = JSON.parse(req.body); dont use this!
+
+    const savedUser = await prisma.user.create({
+      data: user
+    });
+    res.status(200).json(savedUser);
+  }
+  catch ( err ){
+    res.status(400).json({ message: 'Something went wrong'})
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,96 @@
-import axios from 'axios'
+import axios from "axios";
+import Head from "next/head";
+import { useState } from "react";
+import { Button, Container, Form } from "react-bootstrap";
+import prisma from "../lib/prisma";
 
-export default function Home() {
-  
-  axios.get(`/api/hello`).then(console.log);
+export async function getServerSideProps() {
+  const users = await prisma.user.findMany();
+  return {
+    props: { initialUsers: users },
+  };
+}
+
+export default function Home({ initialUsers }) {
+  const [users, setUsers] = useState(initialUsers);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [avatar, setAvatar] = useState("");
+  const [role, setRole] = useState("");
 
   return (
-    <div className='login-container'>
-      <h1>Hello World</h1>
-    </div>
-  )
+    <>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+      <Container style={{ margin: 20 }}>
+        <h3>Signup trial for prisma api</h3>
+        <Form
+          onSubmit={async () => {
+            const body = {
+              firstName,
+              lastName,
+              role,
+              email,
+              avatar,
+            };
+
+            await axios.post("/api/create", { user: body });
+            await setUsers([...users, body]);
+            setFirstName("");
+            setLastName("");
+            setEmail("");
+            setAvatar("");
+            setRole(null);
+          }}
+        >
+          <Form.Group widths="equal">
+            <input
+              type="text"
+              label="First Name"
+              placeholder="First Name"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+            ></input>
+            <br />
+            <input
+              type="text"
+              label="Last Name"
+              placeholder="Last Name"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+            ></input>
+            <br />
+            <input
+              type="text"
+              label="email"
+              placeholder="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></input>
+            <br />
+            <input
+              type="text"
+              label="avatar"
+              placeholder="avatar"
+              value={avatar}
+              onChange={(e) => setAvatar(e.target.value)}
+            ></input>
+            <br />
+            <select
+              onChange={(e) => {
+                setRole(e.target.value.toUpperCase());
+                console.log(role);
+              }}
+            >
+              <option value="Admin">Admin</option>
+              <option value="User">User</option>
+            </select>
+          </Form.Group>
+          <input value="Submit" type="submit" />
+        </Form>
+      </Container>
+    </>
+  );
 }
