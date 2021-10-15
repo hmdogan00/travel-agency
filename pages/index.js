3b62507ce4deda5c91a5136a00f346958f79e057
hmdogan00 <mertdogan@jotform.com>
2021-10-15 10:15:04 +0300

    file order changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 import Head from "next/head";
 import { useState } from "react";
-import { Form, Container, Divider, Table, Header, Image, Button, Icon } from "semantic-ui-react";
+import {
+  Form,
+  Container,
+  Divider,
+  Table,
+  Header,
+  Image,
+  Button,
+  Icon,
+} from "semantic-ui-react";
 import prisma from "../lib/prisma";
 
 export async function getServerSideProps() {
@@ -47,15 +56,14 @@ export default function Home({ initialUsers }) {
               email,
               avatar,
             };
-
-            const res = await axios.post("/api/createUser", { user: body });
-            console.log(res.data)
-            await setUsers([...users, res.data]);
-            setFirstName("");
-            setLastName("");
-            setEmail("");
-            setAvatar("");
-            setRole(null);
+            axios.post("/api/createUser", { user: body }).then( res => {
+              setUsers([...users, res.data]);
+              setFirstName("");
+              setLastName("");
+              setEmail("");
+              setAvatar("");
+              setRole(null);
+            }).catch( err => console.error(err))
           }}
         >
           <Form.Group widths="equal">
@@ -100,46 +108,54 @@ export default function Home({ initialUsers }) {
           <Form.Button>Submit</Form.Button>
         </Form>
         <Divider horizontal>Users</Divider>
-          
-          <Table celled collapsing>
-            <Table.Header>
-              <Table.Row>
-                <Table.HeaderCell>User Name</Table.HeaderCell>
-                <Table.HeaderCell>Email</Table.HeaderCell>
-                <Table.HeaderCell>Action</Table.HeaderCell>
-              </Table.Row>
-            </Table.Header>
-            <Table.Body>
-              { users.map((user, ind) => 
-                <Table.Row key={ind}>
-                  <Table.Cell>
-                    <Header as='h4' image>
-                      <Image src={user.avatar} rounded size='mini'></Image>
-                      <Header.Content>
-                        {`${user.firstName} ${user.lastName}`}
 
-                        <Header.Subheader>{user.role.toUpperCase()}</Header.Subheader>
-                      </Header.Content>
-                    </Header>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Header as='h4'>{user.email}</Header>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <Button animated='fade' color='red' onClick={async () => {
-                      const res = await axios.delete(`/api/deleteUser/${user.id}`)
-                      await setUsers(users.filter(u => u.id !== user.id))
-                    }}>
-                      <Button.Content visible>Delete</Button.Content>
-                      <Button.Content hidden>
-                        <Icon name='user delete'/>
-                      </Button.Content>
-                    </Button>
-                  </Table.Cell>
-                </Table.Row>
-              )}
-            </Table.Body>
-          </Table>
+        <Table celled collapsing>
+          <Table.Header>
+            <Table.Row>
+              <Table.HeaderCell>User Name</Table.HeaderCell>
+              <Table.HeaderCell>Email</Table.HeaderCell>
+              <Table.HeaderCell>Action</Table.HeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {users.map((user, ind) => (
+              <Table.Row key={ind}>
+                <Table.Cell>
+                  <Header as="h4" image>
+                    <Image src={user.avatar} rounded size="mini"></Image>
+                    <Header.Content>
+                      {`${user.firstName} ${user.lastName}`}
+
+                      <Header.Subheader>
+                        {user.role.toUpperCase()}
+                      </Header.Subheader>
+                    </Header.Content>
+                  </Header>
+                </Table.Cell>
+                <Table.Cell>
+                  <Header as="h4">{user.email}</Header>
+                </Table.Cell>
+                <Table.Cell>
+                  <Button
+                    animated="fade"
+                    color="red"
+                    onClick={async () => {
+                      const res = await axios.delete(
+                        `/api/deleteUser/${user.id}`
+                      );
+                      await setUsers(users.filter((u) => u.id !== user.id));
+                    }}
+                  >
+                    <Button.Content visible>Delete</Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="user delete" />
+                    </Button.Content>
+                  </Button>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table>
       </Container>
     </>
   );
