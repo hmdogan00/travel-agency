36eb1b3b65f2410d64db03872125fd48486ba8ec
hmdogan00 <mertdogan@jotform.com>
2021-12-14 15:05:15 +0300

    new backend method

--- pages/api/getUsers.js
+++ pages/api/getUsers.js
@@ -3,12 +3,17 @@ import db from "../../lib/db";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (req, res) => {
   try {
-    db.query("SELECT * FROM Customer", function (err, results, fields) {
-      if ( err ) return res.status(500).json({ message: err})
-      res.status(200).json({ results: results, fields: fields });
-    });
-    return res;
+      db.query("SELECT * FROM Customer", function (err, results, fields) {
+        if (err) {
+          res.statusCode = 401;
+          res.json({ message: err });
+          res.end();
+          return Promise.reject(err);
+        }
+        res.status(200).json({ results: results, fields: fields });
+        return Promise.resolve(res);
+      });
   } catch (e) {
-    return res.status(401).json({ message: e });
+    console.log(e);
   }
 };
