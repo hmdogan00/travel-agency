d9e8e7021b4e911b3377f76c5ab37c131b5da264
Bora Fatih KAZANCI <fatih.kazanci@ug.bilkent.edu.tr>
2021-12-20 20:33:39 +0300

    logooooo yetoooo

--- pages/login.js
+++ pages/login.js
@@ -27,70 +27,72 @@ export default function LoginPage() {
           href="https://cdn.jsdelivr.net/npm/semantic-ui@2/dist/semantic.min.css"
         />
       </Head>
-      <div className="mid-container-logo">
-        <Image alt="logo" src="" fluid />
-        <h1>TripFellas</h1>
-      </div>
-      <div className="mid-container-b">
-        <Container>
-          <Form
-            onSubmit={async () => {
-              const body = {
-                email: email,
-                password: password,
-                role: role
-              };
-              axios
-                .post("/api/userLogin", body)
-                .then((res) => {
-                  setEmail("");
-                  setPassword("");
-                  if (res.data.messageType === "SUCCESS") {
-                    setLoginCredentials({ email: res.data.email });
-                    isLogin(1);
-                  }
-                  else if (res.status === 201) {
-                    isLogin(2);
-                  }
-                })
-                .catch((err) => console.error(err));
-            }}
-          >
-            <Form.Group widths="equal">
-              <div className="mid-container">
-                <Form.Input
-                  fluid
-                  label="Email"
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Form.Input
-                  fluid
-                  label="Password"
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-                <Form.Select
-                  fluid
-                  label="Role"
-                  placeholder="Role"
-                  options={roleOptions}
-                  value={role}
-                  onChange={handleRoleChange}
-                />
-              </div>
-            </Form.Group>
-            <Form.Button>Log in</Form.Button>
-            <Button onClick={() => (window.location.href = "/")}>Go Back</Button>
-          </Form>
-        </Container>
-        {login === 1 && loginCredentials !== null && (
-          <h1>{`Login is successful with email ${loginCredentials.email}`}</h1>
-        )}
-        {login === 2 && <h1>Düzgün şifre gir aq</h1>}
-      </div>
+      <Container className="login">
+        <div className="mid-container-logo">
+          <Image alt="logo" src="" fluid />
+          <h1>TripFellas</h1>
+        </div>
+        <div className="mid-container-b">
+          <Container>
+            <Form
+              onSubmit={async () => {
+                const body = {
+                  email: email,
+                  password: password,
+                  role: role
+                };
+                axios
+                  .post("/api/userLogin", body)
+                  .then((res) => {
+                    setEmail("");
+                    setPassword("");
+                    if (res.data.messageType === "SUCCESS") {
+                      setLoginCredentials({ email: res.data.email });
+                      isLogin(1);
+                    }
+                    else if (res.status === 201) {
+                      isLogin(2);
+                    }
+                  })
+                  .catch((err) => console.error(err));
+              }}
+            >
+              <Form.Group widths="equal">
+                <div className="mid-container">
+                  <Form.Input
+                    fluid
+                    label="Email"
+                    placeholder="Email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                  />
+                  <Form.Input
+                    fluid
+                    label="Password"
+                    placeholder="Password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <Form.Select
+                    fluid
+                    label="Role"
+                    placeholder="Role"
+                    options={roleOptions}
+                    value={role}
+                    onChange={handleRoleChange}
+                  />
+                </div>
+              </Form.Group>
+              <Form.Button>Log in</Form.Button>
+              <Button onClick={() => (window.location.href = "/")}>Go Back</Button>
+            </Form>
+          </Container>
+          {login === 1 && loginCredentials !== null && (
+            <h1>{`Login is successful with email ${loginCredentials.email}`}</h1>
+          )}
+          {login === 2 && <h1>Düzgün şifre gir aq</h1>}
+        </div>
+      </Container>
     </>
   );
 }
