89bcef398c4e18008a6e77c4692bcc04d73123aa
hmdogan00 <58589080+hmdogan00@users.noreply.github.com>
2021-12-20 18:53:22 +0300

    new home new rules

--- scripts/migrate-db.js
+++ scripts/migrate-db.js
@@ -14,35 +14,35 @@ db.connect();
 
 sendQuery(`CREATE TABLE IF NOT EXISTS Customer(
   id INT PRIMARY KEY AUTO_INCREMENT,
-    name TEXT NOT NULL,
+    name VARCHAR(50) NOT NULL,
     gender ENUM('Male','Female'),
     age INT NOT NULL,
     no_of_trips INT DEFAULT 0,
     identity_no INT NOT NULL,
-    phone_no TEXT,
-    email TEXT UNIQUE,
-    password TEXT NOT NULL
+    phone_no VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    password VARCHAR(50) NOT NULL
   )`);
   sendQuery(`CREATE TABLE IF NOT EXISTS Guide(
   guide_id INT PRIMARY KEY AUTO_INCREMENT,
-    name TEXT NOT NULL,
+    name VARCHAR(50) NOT NULL,
     gender ENUM('Male','Female'),
-    location TEXT NOT NULL,
+    location VARCHAR(50) NOT NULL,
     rating FLOAT(10) DEFAULT 0,
     identity_no INT NOT NULL,
-    phone_no TEXT,
-    email TEXT UNIQUE,
-    password TEXT NOT NULL
+    phone_no VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    password VARCHAR(50) NOT NULL
   )`)
   sendQuery(`
 CREATE TABLE IF NOT EXISTS Employee(
   employee_id INT PRIMARY KEY AUTO_INCREMENT,
-    name TEXT NOT NULL,
+    name VARCHAR(50) NOT NULL,
     role ENUM('MANAGER','STAFF'),
     identity_no INT NOT NULL,
-    phone_no TEXT,
-    email TEXT UNIQUE,
-    password TEXT NOT NULL
+    phone_no VARCHAR(50),
+    email VARCHAR(50) UNIQUE,
+    password VARCHAR(50) NOT NULL
   )`)
 
 db.end();
