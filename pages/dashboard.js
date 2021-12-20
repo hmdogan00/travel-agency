ca384c8b65345a846d027e091f2801c1ab2cffb9
hmdogan00 <58589080+hmdogan00@users.noreply.github.com>
2021-12-21 00:07:44 +0300

    getCustomerTours eslint fix and customer dashboard finished

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -13,16 +13,16 @@ function Dashboard() {
       setRole(localStorage.getItem("role"));
       setId(localStorage.getItem('id'));
     }
-    if (!tourArr) {
+    if (role === 'Customer' && !tourArr) {
       axios.get(`/api/getCustomerTours/${id}`).then((res) => {
-        setTourArr(res.results);
+        setTourArr(res.data.result);
       }).catch(e => console.error(e));
     } 
-  }, [tourArr, id]);
+  }, [tourArr, id, role]);
   return (
     <>
       <Navbar activeType="dashboard" />
-      <div style={{margin:'30px'}}>
+      { role === "Customer" && <div style={{margin:'30px'}}>
         <Card.Group>
           <Card
             color="red"
@@ -60,9 +60,21 @@ function Dashboard() {
             meta="Istanbul"
             description="Start Date: 21.10.2021 Start Time: 21.00 End Date: 22.10.2021 End Time: 09.00"
           />
-          {tourArr && tourArr.map((e) => {})}
+          {tourArr && tourArr.map((e, i) => {
+            const startDate = e.start_date.split('T')[0].replaceAll('-','.');
+            const startTime = e.start_date.split('T')[1].substr(0,5);
+            const endDate = e.end_date.split('T')[0].replaceAll('-','.');
+            const endTime = e.end_date.split('T')[1].substr(0,5);
+            return <Card
+              key={`card-${i}`}
+              color="red"
+              header={e.name}
+              meta={e.location}
+              description={`Start Date: ${startDate} Start Time: ${startTime} End Date: ${endDate} End Time: ${endTime}`}
+            />;
+          })}
         </Card.Group>
-      </div>
+      </div>}
     </>
   );
 }
