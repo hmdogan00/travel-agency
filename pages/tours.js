0a7fd3854c177c0203ee75631cf96d7bb0973d99
Bora Fatih KAZANCI <fatih.kazanci@ug.bilkent.edu.tr>
2021-12-21 02:03:44 +0300

    tr <3

--- pages/tours.js
+++ pages/tours.js
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
+import { Flag } from "semantic-ui-react";
 import Navbar from "./Navbar";
 
 function Tours() {
+  const [alist, setAList] = useState([]);
+  useEffect(() => {
+    const arr = [];
+    for (var i = 0; i < 5000; i++) {
+      arr.push(i);
+    }
+    setAList(arr);
+  }, [])
+
   return (
     <>
       <Navbar activeType="tours" />
-
+      {
+        alist.map(() => {
+          return <Flag name='tr' />
+        })
+      }
     </>
   );
 }
