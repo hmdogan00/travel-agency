39770331f2b64f63429b865a6ddae93ca328936c
Bora Fatih KAZANCI <fatih.kazanci@ug.bilkent.edu.tr>
2021-12-23 18:28:30 +0300

    Update tours.js

--- pages/tours.js
+++ pages/tours.js
@@ -1,14 +1,15 @@
+import { stubString } from "lodash";
 import { useEffect, useState } from "react";
-import { Card, Header } from "semantic-ui-react";
+import { Button, Card, CardDescription, Header, Icon } from "semantic-ui-react";
 import Navbar from "./Navbar";
 import { getDateTime } from "./util";
 
 function Tours() {
   const [role, setRole] = useState("");
   const [tourArr, setTourArr] = useState([
-    { name: "Anitkabir", location: "Ankara" },
-    { name: "Kapadokya", location: "Nigde" },
-    { name: "Alt Orman", location: "Sihirdar Vadisi" }
+    { name: "Anitkabir", location: "Ankara", desc: "dashgfjasdbfguhabf fb jhbfjdhasfb fbsdabf jdfbsdja bhjfbasd jbfhjb fhjds bfhjsd fbsdjbfjasfb hjasdbfhj bfj a" },
+    { name: "Kapadokya", location: "Nigde", desc: "gujiasfnk dhs asdjf" },
+    { name: "Alt Orman", location: "Sihirdar Vadisi", desc: " gafdfhadbnf hj bdj bfjh abfsd" }
   ]);
 
   useEffect(() => {
@@ -31,20 +32,41 @@ function Tours() {
                 //const [startDate, startTime] = getDateTime(e.start_date);
                 //const [endDate, endTime] = getDateTime(e.end_date);
                 return (
-                  <Card
-                    key={`card-${i}`}
-                    color="red"
-                    header={e.name}
-                    meta={e.location}
-                    //description={`Start Date: ${startDate} Start Time: ${startTime} End Date: ${endDate} End Time: ${endTime}`}
-                    description="Dasdsadad"
-                  />
+                  <TourCard tour={e} key={`tourCard-${i}`} />
                 );
               })}
           </Card.Group>
         </div>
       )}
     </>
+  );
+}
+
+function TourCard({ tour }) {
+  const [readMore, setReadMore] = useState(false);
+  return (
+    <Card
+      color="red"
+    //description={`Start Date: ${startDate} Start Time: ${startTime} End Date: ${endDate} End Time: ${endTime}`}
+    >
+      <Card.Content>
+        <Card.Header>
+          {tour.name}
+          <Button positive compact circular size="tiny" floated="right">Reserve</Button>
+        </Card.Header>
+        <Card.Meta>{tour.location}</Card.Meta>
+        <Card.Description>
+          {readMore ? tour.desc : `${tour.desc.substring(0, 50)}` + "..."}
+        </Card.Description>
+        <Card.Description>
+          <Button size="mini" compact circular onClick={() => setReadMore(!readMore)}>
+            {readMore ? "Show Less" : "Show More"}
+            &nbsp;&nbsp;
+            {readMore ? <Icon name='minus circle' /> : <Icon name='plus circle' />}
+          </Button>
+        </Card.Description>
+      </Card.Content>
+    </Card>
   );
 }
 
