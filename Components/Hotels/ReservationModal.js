d5a20f55770b7306e5f88c6f48c09cce02df8594
hmdogan00 <58589080+hmdogan00@users.noreply.github.com>
2021-12-29 19:38:07 +0300

    allah olup yazdigim date kodu

--- Components/Hotels/ReservationModal.js
+++ Components/Hotels/ReservationModal.js
@@ -1,23 +1,58 @@
 import { useState, useEffect } from "react";
-import { Modal, Header, Form, Input, Button, Checkbox, Table, Label } from "semantic-ui-react";
+import {
+  Modal,
+  Header,
+  Form,
+  Input,
+  Button,
+  Table,
+  Icon,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "semantic-ui-react";
 //import PaymentModal from "./PaymentModal.js";
 import axios from "axios";
 import { getDateTime, includesNoCase } from "../../util.js";
 
 function ReservationModal({ state, setState, hotel }) {
   const [payModalOpen, setPayModalOpen] = useState(false);
-  const [resPerson, setResPerson] = useState("");
+  const [resPersons, setResPersons] = useState(["1"]);
   const [wantedDate, setWantedDate] = useState("");
   const [wantedEndDate, setWantedEndDate] = useState("");
   const [isRoomSearched, setIsRoomSearched] = useState(false);
   const [roomList, setRoomList] = useState();
 
   const getRooms = () => {
-    axios.get(`/api/hotel/getAvailableRooms?hotel_id=${hotel.hotel_id}&size=${resPerson}&st=${wantedDate}&en=${wantedEndDate}`).then(res => {
-      setIsRoomSearched(true);
-      setRoomList(res.data.results)
-    })
-  }
+    setRoomList(latestArr => latestArr = []);
+    resPersons.forEach(e => {
+      axios
+        .get(
+          `/api/hotel/getAvailableRooms?hotel_id=${hotel.hotel_id}&size=${e}&st=${wantedDate}&en=${wantedEndDate}`
+        )
+        .then(res => {
+          setRoomList(latestArr => latestArr = [...latestArr,...res.data.results]);
+        });
+    });
+    setIsRoomSearched(true);
+  };
+
+  const handleResPerson = (value, index) => {
+    const arr = [];
+    resPersons.forEach((e, i) => {
+      if (i === index) arr.push(value);
+      else arr.push(e);
+    });
+    setResPersons(arr);
+  };
+
+  const addRoom = () => {
+    setResPersons([...resPersons, "0"]);
+  };
+
+  const deleteRoom = index => {
+    setResPersons(resPersons.filter((e, i) => i !== index));
+  };
 
   return (
     <Modal
@@ -31,10 +66,8 @@ function ReservationModal({ state, setState, hotel }) {
         <Modal.Description>
           <Header>Reservation Information</Header>
           <Form>
-            <Form.Field className='reservation-form-field'>
-              <label>
-                Desired start date:
-              </label>
+            <Form.Field className="reservation-form-field">
+              <label>Desired start date:</label>
               <Input
                 type="date"
                 placeholder="Enter start date"
@@ -42,10 +75,8 @@ function ReservationModal({ state, setState, hotel }) {
                 onChange={e => setWantedDate(e.target.value)}
               />
             </Form.Field>
-            <Form.Field className='reservation-form-field'>
-              <label>
-                Desired end date:
-              </label>
+            <Form.Field className="reservation-form-field">
+              <label>Desired end date:</label>
               <Input
                 type="date"
                 placeholder="Enter end date"
@@ -53,23 +84,56 @@ function ReservationModal({ state, setState, hotel }) {
                 onChange={e => setWantedEndDate(e.target.value)}
               />
             </Form.Field>
-            <Form.Field className='reservation-form-field'>
-              <label>Number of people:</label>
-              <Input
-                type="number"
-                placeholder="How many people are you reserving for?"
-                defaultValue={resPerson}
-                onChange={e => setResPerson(e.target.value)}
-              />
-            </Form.Field>
-            <Button color="green" onClick={getRooms}>Get Available Rooms</Button>
+            {resPersons?.map((r, i) => {
+              return (
+                <Form.Field className="reservation-form-field">
+                  <label>Number of people for room {i + 1}:</label>
+                  <Button color="red" onClick={() => deleteRoom(i)} animated>
+                    <Button.Content visible>Delete</Button.Content>
+                    <Button.Content hidden>
+                      <Icon name="close" />
+                    </Button.Content>
+                  </Button>
+                  <Input
+                    type="number"
+                    placeholder="How many people are you reserving for?"
+                    defaultValue={r}
+                    onChange={e => handleResPerson(e.target.value, i)}
+                    style={{ width: "89.7%" }}
+                  />
+                </Form.Field>
+              );
+            })}
+            <Button color="blue" onClick={addRoom} content="Add Room" />
+            <Button color="green" onClick={getRooms}>
+              Get Available Rooms
+            </Button>
           </Form>
-          {isRoomSearched && 
+          {roomList && isRoomSearched && (
             <Table>
-            {roomList?.map(room => {
-
-          })}
-          </Table>}
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Room No</Table.HeaderCell>
+                  <Table.HeaderCell>Floor</Table.HeaderCell>
+                  <Table.HeaderCell>Price</Table.HeaderCell>
+                  <Table.HeaderCell>Size</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <TableBody>
+                {roomList?.map(room => {
+                  if (!isRoomSearched) return;
+                  return (
+                    <TableRow>
+                      <TableCell>{room.hotel_room_no}</TableCell>
+                      <TableCell>{room.room_floor}</TableCell>
+                      <TableCell>{room.room_price}</TableCell>
+                      <TableCell>{room.room_size}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
+          )}
         </Modal.Description>
       </Modal.Content>
       <Modal.Actions>
