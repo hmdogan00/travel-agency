22b3c94abb86fc5130b37be52ad143522fcdf9d6
Bora Fatih KAZANCI <fatih.kazanci@ug.bilkent.edu.tr>
2021-12-29 14:42:00 +0300

    Update getActivityIdeas.js

--- pages/api/employee/getActivityIdeas.js
+++ pages/api/employee/getActivityIdeas.js
@@ -1,11 +1,11 @@
 import db from "../../../lib/db";
 export default async (req, res) => {
-    if(req.method !== 'GET')
+    if (req.method !== 'GET')
         return res.status(400).json({ message: "Method not allowed" });
     try {
-        await db.query(`SELECT AU.name, AU.role, AI.type, AI.name, AI.location, AI.description FROM ActivityIdea AI, create_activity CA,ActivityUser AU
-        WHERE AI.is_accepted = 'waiting' and AI.act_idea_id = CA.act_idea_id and AU.person_id = CA.person_id`, (err, result, fields) =>{
-            if (err) {  
+        await db.query(`SELECT AU.name, AU.role, AI.act_idea_id, AI.type, AI.name, AI.location, AI.description FROM ActivityIdea AI, create_activity CA,ActivityUser AU
+        WHERE AI.is_accepted = 'waiting' and AI.act_idea_id = CA.act_idea_id and AU.person_id = CA.person_id`, (err, result, fields) => {
+            if (err) {
                 res.statusCode = 401;
                 res.json({ message: err });
                 res.end();
