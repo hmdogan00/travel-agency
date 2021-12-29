aa911799c485b1b96bb4bc9f8f137357ef77d18d
yarenyy <yaren.yilmaz@ug.bilkent.edu.tr>
2021-12-29 14:44:54 +0300

    bi daha onaylıyorum

--- pages/api/employee/approveActivityIdea.js
+++ pages/api/employee/approveActivityIdea.js
@@ -1,3 +1,4 @@
+import { result } from "lodash";
 import db from "../../../lib/db";
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req, res) => {
@@ -5,22 +6,29 @@ export default async (req, res) => {
     res.status(400).json({message: 'Method Not Allowed'})
     return;
   }
-  let {id} = req.body;
-  id = parseInt(id);
-  if (!id){
+  const { actID, empID, activity } = req.body;
+  if (!actID){
     res.status(423).json({message: 'No id found!'})
     return;
   }
   try {
-    await db.query(`UPDATE ActivityIdea
+      let answer;
+    db.query(`UPDATE ActivityIdea
                     SET is_accepted ='accepted'
-                    WHERE act_idea_id = ${id}`, (err,result,fields) => {
+                    WHERE act_idea_id = ${actID}`, (err,result,fields) => {
       if (err) {  
-        res.status(400).json({message: err})
-        return;
+        return res.status(400).json({message: err});
       }
-      res.status(200).json({result})
     })
+    db.query(`INSERT INTO accepted (tour_id, employee_id, act_idea_id, start_date, end_date, price, duration, age_restriction) 
+    VALUES (${activity.tour}, ${empID}, ${actID}, '${activity.start}', '${activity.end}', ${activity.price}, '${activity.duration}', ${activity.age})
+    ` , (err,result,fields) => {
+        if (err) {  
+          return res.status(400).json({message: err});
+        }
+        answer = res.status(200).json({message: "SUCCESS"})
+      })
+      return answer;
   }
   catch ( err ){
     res.status(200).json({ message: err, messageType: 'ERROR'})
