e7d10a76710f67c8c6b027f1e30b6609a104ba5d
Mert Yıldırım <yusuf.yildirim@ug.bilkent.edu.tr>
2021-12-30 20:58:38 +0300

    refactor to bookHotel.js

--- pages/api/hotel/bookHotel.js
+++ pages/api/hotel/bookHotel.js
@@ -2,11 +2,7 @@ import db from "../../../lib/db";
 
 export default (req, res) => {
   if (req.method !== "POST") return Promise.reject("Invalid method");
-  let{hotel_room_no} = req.body;
-  let{hotel_id} = req.body;
-  let{person_id} = req.body;
-  let{start_date} = req.body;
-  let{end_date} = req.body;
+  let{hotel_room_no, hotel_id, person_id, start_date, end_date} = req.body;
   try {
     db.query(`INSERT INTO book VALUES(1,${hotel_room_no},${hotel_id},${person_id},'${start_date}','${end_date}', 'waiting','')`, function (err, results, fields) {
       if (err) {
